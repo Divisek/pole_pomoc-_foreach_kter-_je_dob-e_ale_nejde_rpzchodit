4623a9243885537f164de8433c3d5ebc6437a609
Divisek <113785426+Divisek@users.noreply.github.com>
2022-12-15 11:09:55 +0100

    Add files via upload

---
+++ pole_pomocí_foreach_je dobře_ale_nejde_rozchodit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pole= new int[10];
+            pole[0] = 1;
+            for (int posun_indexu = 0; posun_indexu < 10; posun_indexu++)
+            {
+                pole[posun_indexu] = posun_indexu + 1;
+            }
+            foreach (int i in pole)
+                Console.WriteLine("{0}", pole[i]);
+        }
+    }
+}
